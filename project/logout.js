5d8cfe86fcc87463f3f3027a61d93893612b20da
shahd <shahdshaban932@gmail.com>
2025-09-20 00:54:57 +0300

    Delete project directory

--- project/logout.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".nav-link.logout")?.addEventListener("click", function(e) {
-  e.preventDefault();
-  localStorage.removeItem("loggedInUser");
-  window.location.href = "login.html";
-});
